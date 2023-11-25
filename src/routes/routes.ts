bfdc67a4947009ce062ad8d6d8945ecca18455d0
Robson Wendel <robson.costa134@gmail.com>
2023-11-24 22:30:57 -0300

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { register } from "./register";
 import login from "./login";
 import user from "./protegida";
 import authenticate from "../middleware/middleware";
 
 export async function routes(app: FastifyInstance) {
+  app.get("/", (request: FastifyRequest, reply: FastifyReply) => {
+    reply.status(200).send({ mensagem: "API para o desafio" });
+  });
   app.post("/register", register);
   app.post("/login", login);
   app.get("/user", { onRequest: [authenticate] }, user);
